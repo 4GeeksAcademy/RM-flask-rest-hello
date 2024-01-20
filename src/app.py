61007ab1e3d99494a6d7da5e95aebbd554306775
Raul Madero <127880270+Raul-madero@users.noreply.github.com>
2024-01-20 18:45:12 +0000

    models y routes

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, People, Planets, Favorites
 #from models import Person
 
 app = Flask(__name__)
@@ -43,6 +43,64 @@ def handle_hello():
         "msg": "Hello, this is your GET /user response "
     }
 
+    return jsonify(response_body), 200
+
+@app.route('/favorites', methods=['GET'])
+def get_favorites():
+    response_body = {
+        "id": Favorites.id,
+        "user_id": Favorites.user_id,
+        "planets_id": Favorites.planets_id,
+        "people_id": Favorites.people_id
+    }
+    return jsonify(response_body), 200
+
+@app.route('/favorite/people/<int:people_id>', methods=['POST', 'DELETE'])
+def add_or_remove_people_to_favorites(user_id, people_id): #Modifique
+    user = User.query.get_or_404(user_id)
+    if request.method == 'DELETE': 
+          persona_eliminada = user.people_favorite(people_id)
+          db.session.delete(persona_eliminada)
+          db.session.comit()
+          return jsonify({"message": "Persona eliminada de favoritos"})
+    else:
+        nuevo_Favorito = user(people_id=people_id)
+        db.session.add(nuevo_Favorito)
+        db.session.commit()
+        return jsonify({"message": "Persona añadida a favoritos"})
+
+@app.route('/planets', methods=['GET'])
+def get_planets(): #Correcion
+    response_body = {
+        "id": Planets.id,
+        "name": Planets.name,
+        "rotation_period": Planets.rotation_period,
+        "orbital_period": Planets.orbital_period,
+        "diameter": Planets.diameter,
+        "climate": Planets.climate,
+        "gravity": Planets.gravity,
+        "terrain": Planets.terrain,
+        "surface_water": Planets.surface_water,
+        "population": Planets.population,
+        "message": "Desde planets"
+    }
+    return jsonify(response_body), 200
+
+@app.route('/people', methods=['GET'])
+def get_people():
+    response_body = {
+        "id": People.id,
+        "name": People.name,
+        "height": People.height, 
+        "mass": People.mass, 
+        "hair_color": People.hair_color, 
+        "skin_color": People.skin_color, 
+        "eye_color": People.eye_color, 
+        "birth_color": People.birth_year, 
+        "gender": People.gender,
+        "homeworld": People.homeworld, 
+        "message": "Desde People"
+    }
     return jsonify(response_body), 200
 
 # this only runs if `$ python src/app.py` is executed
--- src/models.py
+++ src/models.py
@@ -1,12 +1,90 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+    
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    planets_id = db.Column(db.Integer, db.ForeignKey("planets.id"))
+    people_id = db.Column(db.Integer, db.ForeignKey("people.id"))
 
+    def __repr__(self):
+        return '<Favorites %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "planets_id": self.planets_id, 
+            "people_id": self.people_id
+        }
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(45), nullable=False, unique=False)
+    height = db.Column(db.Integer)
+    mass = db.Column(db.Integer)
+    hair_color = db.Column(db.String(45))
+    skin_color = db.Column(db.String(45))
+    eye_color = db.Column(db.String(45))
+    birth_year = db.Column(db.String(45))
+    gender = db.Column(db.String(45))
+    homeworld = db.Column(db.String(45))
+    favorites = db.relationship("Favorites")
+
+    def __repr__(self):
+        return '<People %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "height": self.height, 
+            "mass": self.mass,
+            "hair_color": self.hair_color, 
+            "skin_color": self.skin_color, 
+            "eye_color": self.eye_color, 
+            "birth_color": self.birth_year, 
+            "gender": self.gender,
+            "homeworld": self.homeworld, 
+        }
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(45), nullable=False, unique=False)
+    rotation_period = db.Column(db.String(45))
+    orbital_period = db.Column(db.String(45))
+    diameter = db.Column(db.String(45))
+    climate = db.Column(db.String(45))
+    gravity = db.Column(db.String(45))
+    terrain = db.Column(db.String(45))
+    surface_water = db.Column(db.String(45))
+    population = db.Column(db.Integer)
+    favorites = db.relationship("Favorites")
+
+    def __repr__(self):
+        return '<Planets %r>' % self.name
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "rotation_period": self.rotation_period, 
+            "orbital_period": self.orbital_period,
+            "diameter": self.diameter,
+            "climate": self.climate,
+            "gravity": self.gravity, 
+            "terrain": self.terrain, 
+            "surface_water": self.surface_water, 
+            "population": self.population
+        }
+    
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorites = db.relationship("Favorites")
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -16,4 +94,6 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+            "is_active": self.is_active
         }
+    
